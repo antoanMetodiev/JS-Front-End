ff45b53a4021799fd02fb6dc38db3b4f23111fa8
Antoan Metodiev <Boksnem@abv.bg>
2024-04-20 22:28:36 +0300

    add solution

---
+++ JS Syntax Fundamentals/JS Syntax Fundamentals - Lab/month-printer.js
@@ -0,0 +1,12 @@
+function solve(day) {
+    let year = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June',
+        7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'};
+    
+    if (year[day]) {
+        console.log(year[day]) 
+    } else {
+        console.log('Error!');
+    }
+}
+
+solve(12);
