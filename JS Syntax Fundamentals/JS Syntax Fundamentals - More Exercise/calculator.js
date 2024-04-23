d1e8a4e71ef1c11644f291de9755f93a64fc9321
Antoan Metodiev <Boksnem@abv.bg>
2024-04-23 22:04:24 +0300

    add solution

---
+++ JS Syntax Fundamentals/JS Syntax Fundamentals - More Exercise/calculator.js
@@ -0,0 +1,8 @@
+function solve(num1, operation, num2) {
+    let operations = { '+': num1 + num2, '-': num1 - num2, '/': num1 / num2, '*': num1 * num2 };
+    console.log(operations[operation].toFixed(2));
+}
+
+solve(25.5,
+    '-',
+    3);
