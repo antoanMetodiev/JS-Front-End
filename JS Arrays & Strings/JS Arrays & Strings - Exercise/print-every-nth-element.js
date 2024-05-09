0f8848ef79d24772b567151e94d67b495eebba03
Antoan Metodiev <Boksnem@abv.bg>
2024-05-09 20:15:16 +0300

    add solution

---
+++ JS Arrays & Strings/JS Arrays & Strings - Exercise/print-every-nth-element.js
@@ -0,0 +1,13 @@
+function solve(numbers, n) {
+    let rotations = numbers.length;
+    let resultArr = [];
+    for (let i = 0; i < rotations; i += n) {
+        if (i % n == 0) {
+            resultArr.push(numbers[i]);
+        }
+    }
+    
+    return resultArr;
+}
+
+console.log(solve(['5', '20', '31', '4', '20'], 2));
