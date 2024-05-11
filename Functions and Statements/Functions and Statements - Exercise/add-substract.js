4cca21e37000bd764b206d2430f81798d15ee08b
Antoan Metodiev <Boksnem@abv.bg>
2024-05-11 19:11:09 +0300

    add solution

---
+++ Functions and Statements/Functions and Statements - Exercise/add-substract.js
@@ -0,0 +1,14 @@
+function solve(num1, num2, num3) {
+    let addedNumbers = function () {
+        return num1 + num2;
+    }
+
+    let substraction = function () {
+        return addedNumbers() - num3;
+    }
+    console.log(substraction());
+}
+
+solve(23,
+    6,
+    10);
