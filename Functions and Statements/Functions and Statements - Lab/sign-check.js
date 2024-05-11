ed56eab7fd819f609a82acb3f57d2373b50f96fa
Antoan Metodiev <Boksnem@abv.bg>
2024-05-11 19:10:26 +0300

    add solution

---
+++ Functions and Statements/Functions and Statements - Lab/sign-check.js
@@ -0,0 +1,6 @@
+function solve(num1, num2, num3) {
+    if ((num1 * num2 * num3) > 0) console.log('Positive');
+    else console.log('Negative');
+}
+
+solve(5, 12, -15);                                  
