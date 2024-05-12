6b05a34667d9eb15ec83b591c92f0df2cf74f8f1
Antoan Metodiev <Boksnem@abv.bg>
2024-05-12 14:49:11 +0300

    add solution

---
+++ Functions and Statements/Functions and Statements - Exercise/factorial-division.js
@@ -0,0 +1,14 @@
+
+function solve(num, divisor) {
+    let recursionResult = 1;
+    function doFactoriel(currentNumber, counter) {
+        return currentNumber * (counter + 1);
+    }
+
+    for (let i = 1; i < num; i++) {
+        recursionResult = doFactoriel(recursionResult, i);
+    }
+    console.log((recursionResult / divisor).toFixed(2));
+}
+
+solve(5, 2);
