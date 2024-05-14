55f894439a8664f4e8fc41408394e1e293633a0f
Antoan Metodiev <Boksnem@abv.bg>
2024-05-14 20:55:45 +0300

    add solution

---
+++ Objects and Classes/Objects and Classes - Lab/address-book.js
@@ -0,0 +1,21 @@
+function solve(addresses) {
+    const addressesData = {};
+    for (const address of addresses) {
+        let [personName, addressName] = address.split(':');
+        addressesData[personName] = addressName;
+    }
+    Object.entries(addressesData).sort((entry1, entry2) => entry1[0].localeCompare(entry2[0]))
+        .forEach(([personName, address]) => console.log(`${personName} -> ${address}`));
+}
+
+solve(['Bob:Huxley Rd',
+    'John:Milwaukee Crossing',
+    'Peter:Fordem Ave',
+    'Bob:Redwing Ave',
+    'George:Mesta Crossing',
+    'Ted:Gateway Way',
+    'Bill:Gateway Way',
+    'John:Grover Rd',
+    'Peter:Huxley Rd',
+    'Jeff:Gateway Way',
+    'Jeff:Huxley Rd']);
