48a70d55f450db842f385204755de371cb517cf5
Antoan Metodiev <Boksnem@abv.bg>
2024-05-14 20:59:44 +0300

    add solution

---
+++ Objects and Classes/Objects and Classes - Exercise/employees.js
@@ -0,0 +1,15 @@
+function solve(employeesData) {
+    const employees = {};
+    employeesData.forEach(el => {
+        employees[el] = el.length;
+    });
+    Object.keys(employees)
+    .forEach(key => console.log(`Name: ${key} -- Personal Number: ${employees[key]}`));
+}
+
+solve([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+]);
