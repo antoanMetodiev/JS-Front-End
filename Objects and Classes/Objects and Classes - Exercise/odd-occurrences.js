e1eee0b92d2bd840455ab644ef90f1a50a092b60
Antoan Metodiev <Boksnem@abv.bg>
2024-05-15 22:19:58 +0300

    add solution

---
+++ Objects and Classes/Objects and Classes - Exercise/odd-occurrences.js
@@ -0,0 +1,16 @@
+function solve(words) {
+    const map = new Map();
+    words.toLowerCase().split(' ').forEach(word => {
+        if (map.has(word)) map.set(word, map.get(word) + 1);
+        else map.set(word, 1);
+    });
+
+    let result = '';
+    Array.from(map.keys()).forEach(key => {
+        if (map.get(key) % 2 != 0) result += key + ' '; 
+    });
+    
+    console.log(result.trim());
+}
+
+solve('Cake IS SWEET is Soft CAKE sweet Food');
