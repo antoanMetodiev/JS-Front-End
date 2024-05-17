4c0ff4fdd8a2b19f6c91f8bfd78fedd865ff462c
Antoan Metodiev <Boksnem@abv.bg>
2024-05-17 20:22:37 +0300

    add solution

---
+++ DOM Introduction/DOM Introduction - Exercise/Table - Search Engine/solution.js
@@ -0,0 +1,18 @@
+function solve() {
+   document.querySelector('#searchBtn').addEventListener('click', onClick);
+
+   function onClick() {
+      let searchedText = document.getElementById('searchField');
+      let trElements = document.querySelectorAll('tbody tr');
+
+      Array.from(document.querySelectorAll('tbody tr')).forEach((tr) => {
+         for (let td of tr.children) {
+            if (td.textContent.includes(searchedText.value)) {
+               td.parentElement.classList.add('select');
+               break;
+            } 
+         }
+      });
+      searchedText.value = '';
+   }
+}
