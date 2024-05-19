31ac57eb2e06617d3fff43d111d9a8d4becb2bc0
Antoan Metodiev <Boksnem@abv.bg>
2024-05-19 22:07:43 +0300

    add solution

---
+++ DOM Introduction/DOM Introduction - Exercise/Generate Report/report.js
@@ -0,0 +1,23 @@
+function generateReport() {
+    let employeeInputEl = document.getElementsByTagName('input');
+
+    const data = []; // array with objects!
+    for (let i = 0; i < employeeInputEl.length; i++) {
+        if (employeeInputEl[i].checked) {
+            let tdElements = document.querySelectorAll(`tr td:nth-of-type(${i + 1})`);
+
+            if (data.length == 0) {
+                tdElements.forEach(td => {
+                    const propName = employeeInputEl[i].parentElement.textContent.toLocaleLowerCase().trim();
+                    data.push({ [propName]: td.textContent });
+                })
+            } else {
+                tdElements.forEach((td, index) => {
+                    const propName = employeeInputEl[i].parentElement.textContent.toLocaleLowerCase().trim();
+                    data[index][propName] = td.textContent;
+                })
+            }
+        }
+    }
+    document.getElementById('output').textContent = JSON.stringify(data, null, 2);
+}
