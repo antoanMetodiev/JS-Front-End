fac9f067ce9a123f78be90a31c09639f41605ae9
Antoan Metodiev <Boksnem@abv.bg>
2024-05-20 20:50:34 +0300

    add solution

---
+++ DOM Manipulation and Events/DOM Manipulation and Events - Lab/Highlight-Active/app.js
@@ -0,0 +1,13 @@
+function focused() {
+    const inputElement = document.getElementsByTagName('input');
+
+    for (const input of inputElement) {
+        input.addEventListener('focus', (event) => {
+            event.target.parentElement.classList.add('focused');
+        });
+
+        input.addEventListener('blur', (event) => {
+            event.target.parentElement.classList.remove('focused');
+        })
+    }
+}
