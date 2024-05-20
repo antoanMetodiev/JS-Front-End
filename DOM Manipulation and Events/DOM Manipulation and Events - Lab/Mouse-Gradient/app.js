03a8c7660daa94aa31340395bb78f9b811a3b608
Antoan Metodiev <Boksnem@abv.bg>
2024-05-20 20:48:55 +0300

    add solution

---
+++ DOM Manipulation and Events/DOM Manipulation and Events - Lab/Mouse-Gradient/app.js
@@ -0,0 +1,5 @@
+function attachGradientEvents() {
+    document.getElementById('gradient').addEventListener('mousemove', (event) => {
+        document.getElementById('result').textContent = Math.floor((event.offsetX / event.target.clientWidth) * 100) + '%';
+    });
+}
