6c901337bc008317963949969d125be9e9c2b4c1
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-05-20 20:51:21 +0300

    Update app.js

--- DOM Manipulation and Events/DOM Manipulation and Events - Lab/Highlight-Active/app.js
+++ DOM Manipulation and Events/DOM Manipulation and Events - Lab/Highlight-Active/app.js
@@ -1,6 +1,5 @@
 function focused() {
     const inputElement = document.getElementsByTagName('input');
-
     for (const input of inputElement) {
         input.addEventListener('focus', (event) => {
             event.target.parentElement.classList.add('focused');
